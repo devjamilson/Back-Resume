cab530ef51a7c999c57f4687dec61d528df181a0
Jamilson Ferreira <83456401+devjamilson@users.noreply.github.com>
2023-04-05 14:30:02 -0300

    problema do cors

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const resumeRoute = require("./routes/routes")
 const bancoDeDados = require("./database/db")
 const dotEnv = require('dotenv')
 dotEnv.config()
+const cors = require('cors')
 
 
 const app = express()
 const porta  =process.env.PORT || 3003
 
 bancoDeDados()
+app.use(cors())
 app.use(express.json())
 app.use("/resume", resumeRoute)
 
